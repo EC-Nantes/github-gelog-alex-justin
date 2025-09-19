fa76317296c842fecf446865dc5d4c1f5bc0ce6e
JusiBridou <juslamarcade@gmail.com>
2025-09-19 17:19:43 +0100

    Version 1 des Monstres

---
+++ ProjetTP/src/main/java/org/centrale/objet/WoE/Monstre.java
@@ -0,0 +1,109 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.centrale.objet.WoE;
+
+/**
+ *
+ * @author Alexandre
+ * @version 1.0
+ */
+public class Monstre {
+    private int ptVie;
+    private int degAtt;
+    private int ptPar;
+    private int pageAtt;
+    private int pagePar;
+    private Point2D pos;
+    
+    public Monstre(int pV, int dA, int pPar, int paAtt, int paPar, Point2D pos) {
+	ptVie = pV;
+	degAtt = dA;
+	ptPar = pPar;
+	pageAtt = paAtt;
+	pagePar = paPar;
+	this.pos = new Point2D(pos);
+    }
+    
+    public Monstre(Monstre m) {
+	ptVie = m.ptVie;
+	degAtt = m.degAtt;
+	ptPar = m.ptPar;
+	pageAtt = m.pageAtt;
+	pagePar = m.pagePar;
+    }
+    
+    public Monstre() {
+	ptVie = 100;
+	degAtt = 10;
+	ptPar = 10;
+	pageAtt = 10;
+	pagePar = 10;
+	pos = new Point2D(0, 0);
+    }
+    
+    public void deplace() {
+	pos.translate(5, 5);
+    }
+    
+    public void affiche() {
+	System.out.println("pas encore intégrée");
+    }
+    
+    public int getPtVie() {
+	return ptVie;
+    }
+    
+    public void setPtVie(int pV) {
+	ptVie = pV;
+    }
+
+    public int getDegAtt() {
+	return degAtt;
+    }
+
+    public int getPtPar() {
+	return ptPar;
+    }
+
+    public int getPageAtt() {
+	return pageAtt;
+    }
+
+    public int getPagePar() {
+	return pagePar;
+    }
+
+    public Point2D getPos() {
+	return pos;
+    }
+
+    public void setDegAtt(int degAtt) {
+	this.degAtt = degAtt;
+    }
+
+    public void setPtPar(int ptPar) {
+	this.ptPar = ptPar;
+    }
+
+    public void setPageAtt(int pageAtt) {
+	this.pageAtt = pageAtt;
+    }
+
+    public void setPagePar(int pagePar) {
+	this.pagePar = pagePar;
+    }
+
+    public void setPos(Point2D pos) {
+	this.pos = pos;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+}
