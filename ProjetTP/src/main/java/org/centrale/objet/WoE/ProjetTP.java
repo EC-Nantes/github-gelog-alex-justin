780675d607d87bcd76a4e3d78de59ad0f8107136
JusiBridou <juslamarcade@gmail.com>
2025-09-19 17:51:10 +0100

    Hi

---
+++ ProjetTP/src/main/java/org/centrale/objet/WoE/ProjetTP.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.centrale.objet.WoE;
+
+/**
+ *
+ * @author jujus
+ */
+public class ProjetTP {
+
+    public static void main(String[] args) {
+        TestWoE Test = new TestWoE();
+        Test.main();
+    }
+}
