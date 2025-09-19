68566f5954d8a6a94758b6733977d9dd053f3258
JusiBridou <juslamarcade@gmail.com>
2025-09-19 17:51:22 +0100

    Tests v1

---
+++ ProjetTP/src/main/java/org/centrale/objet/WoE/TestWoE.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.centrale.objet.WoE;
+
+/**
+ *
+ * @author jujus
+ */
+public class TestWoE {
+    public static void main(){
+        Lapin Alexandre = new Lapin();
+        Point2D p = new Point2D();
+        Paysan Justin = new Paysan("Justin", 101, 50, 50, 50, 50, 50, p);
+        Justin.affiche();
+        Alexandre.affiche();
+        for (int i=0 ; i<3; i++){    
+            Justin.deplace();
+            Alexandre.deplace();
+        }
+        Justin.affiche();
+        Alexandre.affiche();
+    }
+    
+}
