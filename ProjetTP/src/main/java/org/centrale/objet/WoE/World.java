70bde8f977a23fd580978b28ebf0cda0703b079e
JusiBridou <juslamarcade@gmail.com>
2025-09-19 17:32:10 +0100

    Version 1 du monde

---
+++ ProjetTP/src/main/java/org/centrale/objet/WoE/World.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.centrale.objet.WoE;
+
+/**
+ *
+ * @author Alexandre
+ * @version 1.0
+ */
+public class World {
+    public Archer robin;
+    public Paysan peons;
+    public Lapin bugs;
+    
+    public World() {
+	robin = new Archer();
+	peons = new Paysan();
+	bugs = new Lapin();
+    }
+    
+    public void creerMondeAlea() {
+	//
+    }
+}
