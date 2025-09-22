4fe8045b3ea81cf44511626a9c51d1fba0c10c01
JusiBridou <juslamarcade@gmail.com>
2025-09-22 15:00:42 +0200

    Version 1 des Bulbis

---
+++ ProjetTP/src/main/java/org/centrale/objet/WoE/Bulbi.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.centrale.objet.WoE;
+
+/**
+ *
+ * @author jujus
+ * 
+ * L'objectif c'est que les bulbis soient une espèce de mage avec une attaque fouet liane qui peut évoluer pour devenir une tempete verte.
+ * les bulbis ont un nombre de PP par défaut, ils en utilisent 1 à chaque attaque 
+ */
+public class Bulbi extends Personnage {
+    private int PP;
+
+    public Bulbi(int PP, String n, int pV, int dA, int pPar, int paAtt, int paPar, int dMax, Point2D p) {
+        super(n, pV, dA, pPar, paAtt, paPar, dMax, p);
+        this.PP = PP;
+    }
+
+    public Bulbi(int PP, Personnage perso) {
+        super(perso);
+        this.PP = PP;
+    }
+
+    public Bulbi(int PP) {
+        this.PP = PP;
+    }
+
+    public Bulbi(String n, int pV, int dA, int pPar, int paAtt, int paPar, int dMax, Point2D p) {
+        super(n, pV, dA, pPar, paAtt, paPar, dMax, p);
+    }
+
+    public Bulbi(Personnage perso) {
+        super(perso);
+    }
+
+    public Bulbi() {
+    }
+    
+    
+    
+}
