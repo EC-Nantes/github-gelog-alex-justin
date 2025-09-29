b75662328153f65376198fbd192d262cafbdc3cc
JusiBridou <juslamarcade@gmail.com>
2025-09-29 15:03:46 +0200

    Complétion de la JavaDoc

--- ProjetTP/src/main/java/org/centrale/objet/WoE/Creature.java
+++ ProjetTP/src/main/java/org/centrale/objet/WoE/Creature.java
@@ -88,96 +88,96 @@ public class Creature {
     }
     
     /**
-     *
-     * @return
+     * Guetteur de points vie
+     * @return le nombre de points de vie
      */
     public int getPtVie() {
 	return ptVie;
     }
     
-    /**
-     *
-     * @param pV
+    /** 
+     * Setter de point de vie
+     * @param pV le nouveau nombre de points de vie
      */
     public void setPtVie(int pV) {
 	ptVie = pV;
     }
 
     /**
-     *
-     * @return
+     * Guetteur de la quantité de dégâts
+     * @return le nombre de dégâts infligés
      */
     public int getDegAtt() {
 	return degAtt;
     }
 
     /**
-     *
-     * @return
+     * Getter du nombre de points de parade
+     * @return les points de parade
      */
     public int getPtPar() {
 	return ptPar;
     }
 
     /**
-     *
-     * @return
+     * le gettter du pourcentage d'attaque
+     * @return le pourcentage de réussite d'attaque
      */
     public int getPageAtt() {
 	return pageAtt;
     }
 
     /**
-     *
-     * @return
+     * le gettter du pourcentage de parade
+     * @return le pourcentage de réussite de parade
      */
     public int getPagePar() {
 	return pagePar;
     }
 
     /**
-     *
-     * @return
+     * le gettter de la position
+     * @return la position de la créature
      */
     public Point2D getPos() {
 	return pos;
     }
 
     /**
-     *
-     * @param degAtt
+     * le setter de la quantité de dégâts infligés
+     * @param degAtt la quantité de dégâts
      */
     public void setDegAtt(int degAtt) {
 	this.degAtt = degAtt;
     }
 
     /**
-     *
-     * @param ptPar
+     * le setter de la quantité de dégâts parés
+     * @param ptPar les points de parade
      */
     public void setPtPar(int ptPar) {
 	this.ptPar = ptPar;
     }
 
     /**
-     *
-     * @param pageAtt
+     * le setter du pourcentage d'attaque réussie
+     * @param pageAtt le pourcentage d'attaque réussie
      */
     public void setPageAtt(int pageAtt) {
 	this.pageAtt = pageAtt;
     }
 
-    /**
-     *
-     * @param pagePar
+    /** 
+     * le setteur du pourcentage de parade réussie
+     * @param pagePar le pourcentage de parade réussie
      */
     public void setPagePar(int pagePar) {
 	this.pagePar = pagePar;
     }
 
     /**
-     *
-     * @param pos
+     * le setter de la position
+     * @param pos la nouvelle position
      */
     public void setPos(Point2D pos) {
 	this.pos = pos;
