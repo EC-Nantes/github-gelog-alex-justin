a93d396c6a2fd346c6b3018c9e12e3953dc8a9bd
JusiBridou <juslamarcade@gmail.com>
2025-10-13 17:04:26 +0200

    Implémentation de la classe Element de jeu → Ajout de pos dedans

--- ProjetTP/src/main/java/org/centrale/objet/WoE/Epee.java
+++ ProjetTP/src/main/java/org/centrale/objet/WoE/Epee.java
@@ -16,9 +16,10 @@ public class Epee extends Objet {
      * Constructeur de base d'une épée
      */
     public Epee() {
-    ptAttaque = 100;
-    ptDurabilite = 100;
-    super.setNom("Epée");
+        super();
+        ptAttaque = 100;
+        ptDurabilite = 100;
+        super.setNom("Epée");
     }
     
     /**
@@ -26,11 +27,12 @@ public class Epee extends Objet {
      * 
      * @param nom nom de l'épée
      * @param ptA Nombre de dégats
+     * @param pos position de l'objet
      */
-    public Epee(String nom, int ptA) {
-    ptAttaque = ptA;
-    ptDurabilite = 100;
-    super.setNom(nom);
+    public Epee(String nom, int ptA, Point2D pos) {
+        super(nom, pos);
+        ptAttaque = ptA;
+        ptDurabilite = 100;
     }
     
     /**
@@ -38,11 +40,12 @@ public class Epee extends Objet {
      * @param nom Nom de l'épée
      * @param ptA Nombre de dégats
      * @param ptD Point de durabilité
+     * @param pos position de l'épée
      */
-    public Epee(String nom, int ptA, int ptD) {
-    ptAttaque = ptA;
-    ptDurabilite = ptD;
-    super.setNom(nom);
+    public Epee(String nom, int ptA, int ptD, Point2D pos) {
+        super(nom, pos);
+        ptAttaque = ptA;
+        ptDurabilite = ptD;
     }
     
     
@@ -53,10 +56,10 @@ public class Epee extends Objet {
      * @param c La créature attaquée
      */
     public void utilisation(Guerrier g, Creature c) {
-    int degAtt_base = g.getDegAtt();
-    g.setDegAtt(degAtt_base + ptAttaque);
-    g.combattre(c);
-    g.setDegAtt(degAtt_base);
+        int degAtt_base = g.getDegAtt();
+        g.setDegAtt(degAtt_base + ptAttaque);
+        g.combattre(c);
+        g.setDegAtt(degAtt_base);
     }
 
     /**
@@ -64,7 +67,7 @@ public class Epee extends Objet {
      * @return les points de dégâts ingligés par l'épee
      */
     public int getPtAttaque() {
-    return ptAttaque;
+        return ptAttaque;
     }
 
     /**
@@ -72,7 +75,7 @@ public class Epee extends Objet {
      * @param ptAttaque la nouvelle quantité de points de dégâts
      */
     public void setPtAttaque(int ptAttaque) {
-    this.ptAttaque = ptAttaque;
+        this.ptAttaque = ptAttaque;
     }
 
     /**
@@ -80,7 +83,7 @@ public class Epee extends Objet {
      * @return la durabilité de l'épee
      */
     public int getPtDurabilite() {
-    return ptDurabilite;
+        return ptDurabilite;
     }
 
     /**
@@ -88,7 +91,7 @@ public class Epee extends Objet {
      * @param ptDurabilite la nouvelle durabilité
      */
     public void setPtDurabilite(int ptDurabilite) {
-    this.ptDurabilite = ptDurabilite;
+        this.ptDurabilite = ptDurabilite;
     }
 
     /**
@@ -97,6 +100,8 @@ public class Epee extends Objet {
      */
     public Epee(Epee baatonn) {
         super(baatonn);
+        this.ptAttaque = baatonn.ptAttaque;
+        this.ptDurabilite = baatonn.ptDurabilite;
     }
     
     
