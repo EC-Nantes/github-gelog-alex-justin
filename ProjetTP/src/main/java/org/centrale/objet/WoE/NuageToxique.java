92442d93f5bf689a4e10aedcc68ba110341d9284
alexElArte <alexandre>
2025-10-17 14:37:15 +0200

    complétion de la sauvegarde et JavaDoc de NuageToxique

--- ProjetTP/src/main/java/org/centrale/objet/WoE/NuageToxique.java
+++ ProjetTP/src/main/java/org/centrale/objet/WoE/NuageToxique.java
@@ -13,6 +13,12 @@ public class NuageToxique extends Objet implements Deplacable, Combattant {
 	private int rayon;            // rayon d’effet du nuage
 	private int degats;           // dégâts infligés aux créatures proches
 	
+	/**
+	 * Constructeur d'un nuage toxique
+	 * @param pos
+	 * @param rayon
+	 * @param degats
+	 */
 	public NuageToxique(Point2D pos, int rayon, int degats) {
 		super();
                 super.setNom("Nuage Toxique");
@@ -21,6 +27,9 @@ public class NuageToxique extends Objet implements Deplacable, Combattant {
 		this.degats = degats;
 	}
 	
+	/**
+	 * Constructeur d'un nuage toxique
+	 */
 	public NuageToxique(){
 		super();
                 super.setNom("Nuage Toxique");;
@@ -28,22 +37,39 @@ public class NuageToxique extends Objet implements Deplacable, Combattant {
 		this.degats = 5;
 	}
 	
+	/**
+	 * Constructeur de copie d'un nuage toxique
+	 * @param nuto
+	 */
 	public NuageToxique(NuageToxique nuto){
 		super(nuto);
 		this.rayon = nuto.rayon;
 		this.degats = nuto.degats;
 	}
 
+	/**
+	 * Déplacement libre d'un nuage toxique
+	 * Il peut passer sur des créatures, des objets ou à travers d'autres éléments
+	 * @param w Le monde dans lequel il se déplace
+	 */
 	@Override
 	public void deplace(World w) {
 		Random rand = new Random();
 		
-		int dx = rand.nextInt(5);
-		int dy = rand.nextInt(5);
+		Point2D pos = super.getPos();
+		int x = pos.getX();
+		int y = pos.getY();
+		int dx;
+		int dy;
+		int max_x = w.getWorld_max_x();
+		int max_y = w.getWorld_max_y();
+
 		
-		dx = dx - 2;
-		dy = dy - 2;
-		
+		do {
+			dx = rand.nextInt(5) - 2;
+			dy = rand.nextInt(5) - 2;
+		} while ((x+dx)<0 || (y+dy)<0 || (x+dx)>= max_x || (y+dy)>= max_y);
+
 		// on s’assure qu’il ne bouge que d’une case ou deux casesà la fois
 		if (Math.abs(dx) > 3 || Math.abs(dy) > 3) {
 			throw new IllegalArgumentException("Le nuage ne peut se déplacer que d'une case à la fois !");
@@ -52,6 +78,10 @@ public class NuageToxique extends Objet implements Deplacable, Combattant {
 		System.out.println("☁️  Le nuage toxique se déplace en " + super.getPos());
 	}
 
+	/**
+	 * Mise en place des dégats sur les créatures dans le rayon d'action
+	 * @param c La créature à infliger les dégats (si elle est dans le rayon d'action)
+	 */
 	@Override
 	public void combattre(Creature c) {
 		// Vérifie la distance entre le nuage et la créature
@@ -68,9 +98,47 @@ public class NuageToxique extends Objet implements Deplacable, Combattant {
 		}
 	}
 
+	/**
+	 * Fonction override
+	 * @return Les informations du nuage
+	 */
 	@Override
 	public String toString() {
 		return "NuageToxique en " + super.getPos() + " (rayon=" + rayon + ", dégâts=" + degats + ")";
 	}
+
+	/**
+	 * Getter du rayon
+	 * @return Le rayon
+	 */
+	public int getRayon() {
+		return rayon;
+	}
+
+	/**
+	 * Setter du rayon
+	 * @param rayon Le rayon
+	 */
+	public void setRayon(int rayon) {
+		this.rayon = rayon;
+	}
+
+	/**
+	 * Getter des dégats
+	 * @return Les dégats positifs ou négatifs
+	 */
+	public int getDegats() {
+		return degats;
+	}
+
+	/**
+	 * Setter des dégats
+	 * @param degats Les dégats positifs ou négatifs
+	 */
+	public void setDegats(int degats) {
+		this.degats = degats;
+	}
+
+	
 }
 
--- ProjetTP/src/main/java/org/centrale/objet/WoE/Sauvegarde.java
+++ ProjetTP/src/main/java/org/centrale/objet/WoE/Sauvegarde.java
@@ -122,6 +122,7 @@ public class Sauvegarde {
 			String line = "Joueur ";
 			line += j.getNomJoueur();
 			line += " ";
+			file.write(line);
 			writeCreature(file, j.getPerso());
 			
 		} catch (FileNotFoundException ex) {
@@ -135,7 +136,7 @@ public class Sauvegarde {
 				file.close();
 			}
 			} catch (IOException ex) {
-			ex.printStackTrace();
+				ex.printStackTrace();
 			}
 		}
 		
@@ -168,197 +169,197 @@ public class Sauvegarde {
 			
 			switch (element) {
 				case "Joueur":
-				temp = tokenizer.nextToken();
-				name = tokenizer.nextToken();
-				nom_joueur = name;
+					temp = tokenizer.nextToken();
+					name = tokenizer.nextToken();
+					nom_joueur = name;
+					
+					if (temp.equals("Guerrier") && tokenizer.countTokens() == 10) {
+						for (int  i = 0; i < 10; i++) {
+							args[i] = Integer.parseInt(tokenizer.nextToken());
+						}
+
+						pos = new Point2D(args[8], args[9]);
+						Guerrier g = new Guerrier(name, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], pos);
+						perso_joueur = (Personnage)g;
+					} else if (temp.equals("Archer") && tokenizer.countTokens() == 9) {
+						for (int  i = 0; i < 9; i++) {
+							args[i] = Integer.parseInt(tokenizer.nextToken());
+						}
+
+						pos = new Point2D(args[6], args[7]);
+						Archer a = new Archer(name, args[0], args[1], args[2], args[3], args[4], args[5], pos, args[8]);
+						perso_joueur = (Personnage)a;
+					}
+					
+					break;
+				case "Largeur":
+					if (tokenizer.countTokens() != 1) break;
+					largeur = Integer.parseInt(tokenizer.nextToken());
+					break;
 				
-				if (temp.equals("Guerrier") && tokenizer.countTokens() == 10) {
+				case "Longueur":
+					if (tokenizer.countTokens() != 1) break;
+					longueur = Integer.parseInt(tokenizer.nextToken());
+					break;
+				
+				case "Guerrier":
+					if (tokenizer.countTokens() != 11) break;
+					name = tokenizer.nextToken();
 					for (int  i = 0; i < 10; i++) {
-					args[i] = Integer.parseInt(tokenizer.nextToken());
+						args[i] = Integer.parseInt(tokenizer.nextToken());
 					}
 
 					pos = new Point2D(args[8], args[9]);
 					Guerrier g = new Guerrier(name, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], pos);
-					perso_joueur = (Personnage)g;
-				} else if (temp.equals("Archer") && tokenizer.countTokens() == 9) {
+					list_creature.add(g);
+					break;
+				
+				case "Archer":
+					if (tokenizer.countTokens() != 10) break;
+					name = tokenizer.nextToken();
 					for (int  i = 0; i < 9; i++) {
-					args[i] = Integer.parseInt(tokenizer.nextToken());
+						args[i] = Integer.parseInt(tokenizer.nextToken());
 					}
 
 					pos = new Point2D(args[6], args[7]);
 					Archer a = new Archer(name, args[0], args[1], args[2], args[3], args[4], args[5], pos, args[8]);
-					perso_joueur = (Personnage)a;
-				}
-				
-				break;
-				case "Largeur":
-				if (tokenizer.countTokens() != 1) break;
-				largeur = Integer.parseInt(tokenizer.nextToken());
-				break;
-				
-				case "Longueur":
-				if (tokenizer.countTokens() != 1) break;
-				longueur = Integer.parseInt(tokenizer.nextToken());
-				break;
-				
-				case "Guerrier":
-				if (tokenizer.countTokens() != 11) break;
-				name = tokenizer.nextToken();
-				for (int  i = 0; i < 10; i++) {
-					args[i] = Integer.parseInt(tokenizer.nextToken());
-				}
-
-				pos = new Point2D(args[8], args[9]);
-				Guerrier g = new Guerrier(name, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], pos);
-				list_creature.add(g);
-				break;
-				
-				case "Archer":
-				if (tokenizer.countTokens() != 10) break;
-				name = tokenizer.nextToken();
-				for (int  i = 0; i < 9; i++) {
-					args[i] = Integer.parseInt(tokenizer.nextToken());
-				}
-
-				pos = new Point2D(args[6], args[7]);
-				Archer a = new Archer(name, args[0], args[1], args[2], args[3], args[4], args[5], pos, args[8]);
-				list_creature.add(a);
-				break;
+					list_creature.add(a);
+					break;
 		
 				case "Paysan":
-				if (tokenizer.countTokens() != 9) break;
-				name = tokenizer.nextToken();
-				for (int  i = 0; i < 8; i++) {
-					args[i] = Integer.parseInt(tokenizer.nextToken());
-				}
-		
-				pos = new Point2D(args[6], args[7]);
-				Paysan p = new Paysan(name, args[0], args[1], args[2], args[3], args[4], args[5], pos);
-				list_creature.add(p);
-				break;
+					if (tokenizer.countTokens() != 9) break;
+					name = tokenizer.nextToken();
+					for (int  i = 0; i < 8; i++) {
+						args[i] = Integer.parseInt(tokenizer.nextToken());
+					}
+			
+					pos = new Point2D(args[6], args[7]);
+					Paysan p = new Paysan(name, args[0], args[1], args[2], args[3], args[4], args[5], pos);
+					list_creature.add(p);
+					break;
 		
 				case "Loup":
-				if (tokenizer.countTokens() != 7) break;
-				for (int  i = 0; i < 7; i++) {
-					args[i] = Integer.parseInt(tokenizer.nextToken());
-				}
+					if (tokenizer.countTokens() != 7) break;
+					for (int  i = 0; i < 7; i++) {
+						args[i] = Integer.parseInt(tokenizer.nextToken());
+					}
 
-				pos = new Point2D(args[5], args[6]);
-				Loup lo = new Loup(args[0], args[1], args[2], args[3], args[4], pos);
-				list_creature.add(lo);
-				break;
+					pos = new Point2D(args[5], args[6]);
+					Loup lo = new Loup(args[0], args[1], args[2], args[3], args[4], pos);
+					list_creature.add(lo);
+					break;
 		
 				case "Lapin":
-				if (tokenizer.countTokens() != 7) break;
-				for (int  i = 0; i < 7; i++) {
-					args[i] = Integer.parseInt(tokenizer.nextToken());
-				}
+					if (tokenizer.countTokens() != 7) break;
+					for (int  i = 0; i < 7; i++) {
+						args[i] = Integer.parseInt(tokenizer.nextToken());
+					}
 
-				pos = new Point2D(args[5], args[6]);
-				Lapin la = new Lapin(args[0], args[1], args[2], args[3], args[4], pos);
-				list_creature.add(la);
-				break;
+					pos = new Point2D(args[5], args[6]);
+					Lapin la = new Lapin(args[0], args[1], args[2], args[3], args[4], pos);
+					list_creature.add(la);
+					break;
 
 				case "Bulbi":
-				if (tokenizer.countTokens() != 8) break;
-				for (int  i = 0; i < 8; i++) {
-					args[i] = Integer.parseInt(tokenizer.nextToken());
-				}
+					if (tokenizer.countTokens() != 8) break;
+					for (int  i = 0; i < 8; i++) {
+						args[i] = Integer.parseInt(tokenizer.nextToken());
+					}
 
-				pos = new Point2D(args[7], args[8]);
-				Bulbi b = new Bulbi(args[0], args[1], args[2], args[3], args[4], args[5], pos);
-				list_creature.add(b);
-				break;
+					pos = new Point2D(args[7], args[8]);
+					Bulbi b = new Bulbi(args[0], args[1], args[2], args[3], args[4], args[5], pos);
+					list_creature.add(b);
+					break;
 
 				case "PotionSoin":
-				if (tokenizer.countTokens() != 4 && !inventaire) break;
-				if (tokenizer.countTokens() != 2 && inventaire) break;
-				name = tokenizer.nextToken();
-				args[0] = Integer.parseInt(tokenizer.nextToken());
-				
-				PotionSoin po;
-				if (!inventaire) {
-								for (int  i = 1; i < 3; i++) {
-					args[i] = Integer.parseInt(tokenizer.nextToken());
+					if (tokenizer.countTokens() != 4 && !inventaire) break;
+					if (tokenizer.countTokens() != 2 && inventaire) break;
+					name = tokenizer.nextToken();
+					args[0] = Integer.parseInt(tokenizer.nextToken());
+					
+					PotionSoin po;
+					if (!inventaire) {
+						for (int  i = 1; i < 3; i++) {
+							args[i] = Integer.parseInt(tokenizer.nextToken());
+						}
+									
+						pos = new Point2D(args[1], args[2]);
+						po = new PotionSoin(name, args[0], pos);
+						list_objet.add(po);
+					} else {
+						po = new PotionSoin(name, args[0]);
+						list_objet_inventaire.add(po);
 					}
-								
-								pos = new Point2D(args[1], args[2]);
-					po = new PotionSoin(name, args[0], pos);
-					list_objet.add(po);
-				} else {
-					po = new PotionSoin(name, args[0]);
-					list_objet_inventaire.add(po);
-				}
-				
-				
-				break;
+					
+					
+					break;
 
 				case "Epee":
-				if (tokenizer.countTokens() != 5 && !inventaire) break;
-				if (tokenizer.countTokens() != 3 && inventaire) break;
-				name = tokenizer.nextToken();
-				for (int  i = 0; i < 2; i++) {
-					args[i] = Integer.parseInt(tokenizer.nextToken());
-				}
-
-				Epee sw;
-				if (!inventaire) {
-								for (int  i = 2; i < 4; i++) {
-					args[i] = Integer.parseInt(tokenizer.nextToken());
+					if (tokenizer.countTokens() != 5 && !inventaire) break;
+					if (tokenizer.countTokens() != 3 && inventaire) break;
+					name = tokenizer.nextToken();
+					for (int  i = 0; i < 2; i++) {
+						args[i] = Integer.parseInt(tokenizer.nextToken());
 					}
-								pos = new Point2D(args[2], args[3]);
-					sw = new Epee(name, args[0], args[1], pos);
-					list_objet.add(sw);
-				} else {
-					sw = new Epee(name, args[0], args[1]);
-					list_objet_inventaire.add(sw);
-				}
-				
-				break;
+
+					Epee sw;
+					if (!inventaire) {
+									for (int  i = 2; i < 4; i++) {
+						args[i] = Integer.parseInt(tokenizer.nextToken());
+						}
+									pos = new Point2D(args[2], args[3]);
+						sw = new Epee(name, args[0], args[1], pos);
+						list_objet.add(sw);
+					} else {
+						sw = new Epee(name, args[0], args[1]);
+						list_objet_inventaire.add(sw);
+					}
+					
+					break;
 			
 				case "Nourriture":
-				if (tokenizer.countTokens() != 6 && !inventaire) break;
-				if (tokenizer.countTokens() != 4 && inventaire) break;
-				name = tokenizer.nextToken();
-				temp = tokenizer.nextToken();
-				
-				for (int  i = 0; i < 2; i++) {
-					args[i] = Integer.parseInt(tokenizer.nextToken());
-				}
-
-				Nourriture n;
-				if (!inventaire) {
-								for (int  i = 2; i < 4; i++) {
-					args[i] = Integer.parseInt(tokenizer.nextToken());
+					if (tokenizer.countTokens() != 6 && !inventaire) break;
+					if (tokenizer.countTokens() != 4 && inventaire) break;
+					name = tokenizer.nextToken();
+					temp = tokenizer.nextToken();
+					
+					for (int  i = 0; i < 2; i++) {
+						args[i] = Integer.parseInt(tokenizer.nextToken());
 					}
-					pos = new Point2D(args[2], args[3]);
-					n = new Nourriture(name, args[0], args[1], temp, pos);
+
+					Nourriture n;
+					if (!inventaire) {
+									for (int  i = 2; i < 4; i++) {
+						args[i] = Integer.parseInt(tokenizer.nextToken());
+						}
+						pos = new Point2D(args[2], args[3]);
+						n = new Nourriture(name, args[0], args[1], temp, pos);
+						list_objet.add(n);
+					} else {
+						n = new Nourriture(name, args[0], args[1], temp);
+						list_objet_inventaire.add(n);
+					}
+					
 					list_objet.add(n);
-				} else {
-					n = new Nourriture(name, args[0], args[2], temp);
-					list_objet_inventaire.add(n);
-				}
-				
-				list_objet.add(n);
-				break;
+					break;
 
 				case "NuageToxique":
-				if (tokenizer.countTokens() != 4) break;
-				for (int  i = 0; i < 4; i++) {
-					args[i] = Integer.parseInt(tokenizer.nextToken());
-				}
+					if (tokenizer.countTokens() != 4) break;
+					for (int  i = 0; i < 4; i++) {
+						args[i] = Integer.parseInt(tokenizer.nextToken());
+					}
 
-							pos = new Point2D(args[3], args[4]);
-				NuageToxique nt = new NuageToxique(pos, args[0], args[1]);
-				list_objet.add(nt);
-				break;
+					pos = new Point2D(args[3], args[4]);
+					NuageToxique nt = new NuageToxique(pos, args[0], args[1]);
+					list_objet.add(nt);
+					break;
 				
 				default:
 				break;
 
 			}
 			} catch (NumberFormatException e) {
-			e.printStackTrace();
+				e.printStackTrace();
 			}
 		}
 	}
@@ -370,6 +371,8 @@ public class Sauvegarde {
 		line += c.getPtPar() + " ";
 		line += c.getPageAtt()+ " ";
 		line += c.getPagePar() + " ";
+
+		boolean write_line = true;
 		
 		
 		switch (classe) {
@@ -389,6 +392,7 @@ public class Sauvegarde {
 				line = ((Personnage)c).getNom() + " " + line;
 				break;
 			default:
+				write_line = false;
 				break;
 		}
 		
@@ -397,14 +401,75 @@ public class Sauvegarde {
 		line += p.getX() + " " + p.getY() + "\n";
 		line = classe + " " + line;
 		
-		f.write(line);
+		if (write_line) f.write(line);
 	}
 	
-	private void writeObjet(BufferedWriter f, Objet o) {
-		//
+	private void writeObjet(BufferedWriter f, Objet o) throws IOException {
+		String classe = c.getClass().getSimpleName();
+		String line = classe + " ";
+
+		Point2D pos = o.getPos();
+		boolean write_line = true;
+
+		switch (classe) {
+			case "Epee":
+				line +=  o.getNom() + " ";
+				line += ((Epee)o).getPtAttaque() + " ";
+				line += ((Epee)o).getPtDurabilite() + " ";
+				line += pos.getX() + " " + pos.getY() + "\n";
+				break;
+			case "PotionSoin":
+				line +=  o.getNom() + " ";
+				line += ((PotionSoin)o).getPtRegen() + " ";
+				line += pos.getX() + " " + pos.getY() + "\n";
+				break;
+			case "NuageToxique":
+				line += pos.getX() + " " + pos.getY() + " ";
+				line += ((NuageToxique)o).getRayon() + " ";
+				line += ((NuageToxique)o).getDegats() + "\n";
+				break;
+			case "Nourriture":
+				line += ((Nourriture)o).getNom() + " ";
+				line += ((Nourriture)o).getIntensite() + " ";
+				line += ((Nourriture)o).getDureeEffet() + " ";
+				line += ((Nourriture)o).getStatCible() + " ";
+				line += pos.getX() + " " + pos.getY() + "\n";
+				break;
+			default:
+				write_line = false;
+				break;
+		}
+
+		if (write_line) file.write(line);
 	}
 	
 	private void writeInventaire(BufferedWriter f, Objet o) {
-		//
+		String classe = c.getClass().getSimpleName();
+		String line = "Inventaire " + classe + " ";
+
+		boolean write_line = true;
+
+		switch (classe) {
+			case "Epee":
+				line +=  o.getNom() + " ";
+				line += ((Epee)o).getPtAttaque() + " ";
+				line += ((Epee)o).getPtDurabilite() + "\n";
+				break;
+			case "PotionSoin":
+				line +=  o.getNom() + " ";
+				line += ((PotionSoin)o).getPtRegen() + "\n";
+				break;
+			case "Nourriture":
+				line += ((Nourriture)o).getNom() + " ";
+				line += ((Nourriture)o).getIntensite() + " ";
+				line += ((Nourriture)o).getDureeEffet() + " ";
+				line += ((Nourriture)o).getStatCible() + "\n";
+				break;
+			default:
+				write_line = false;
+				break;
+		}
+
+		if (write_line) file.write(line);
 	}
 }
